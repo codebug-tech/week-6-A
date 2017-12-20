d5cee848fe0b22950e463551f4aa71c5670e1dd5
Your Name <cbetack@gmail.com>
2017-12-19 20:42:13 -0600

    js changes

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
 //////////////////////////////////////////////////////////////////////////
   // Write the `summertime` function below.
-
+// function summertime () {
+//   return "It's summertime baby!";
+// };
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
   // console.log(summertime())
 
 //////////////////////////////////////////////////////////////////////////
-  // Write `addFive` below. 
-
+  // Write `addFive` below.
+// function addFive (num1) {
+//   var sum = num1 + 5;
+//   return sum;
+// }
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
   // console.log(addFive(10))
 //////////////////////////////////////////////////////////////////////////
   // Write `double` below.
-
+// function double (num1) {
+//   var product = num1 * 2;
+//   return product;
+// }
 
 
   // Uncomment the line below, and look in the console to see if your function worked!
   // console.log(double(6));
 //////////////////////////////////////////////////////////////////////////
   // Write `greetMe` below.
-
+// function greetMe (name) {
+// return "Hello, " + name;
+// }
 
 
 
@@ -31,35 +41,53 @@ document.addEventListener('DOMContentLoaded', function() {
   // console.log(greetMe("Carl"));
 //////////////////////////////////////////////////////////////////////////
   // Write `isItLessThanTen` below.
+// function isItLessThanTen (num) {
+//   if (num <= 10) {
+//     return "Your number is less than 10";
+//   } else if (num > 10) {
+//     return "Your number is greater than 10";
+//   }
+// }
 
 
-
-
-  // Uncomment the lines below, and look in the console to see if your function worked!
-  // console.log(isItLessThanTen(3));
+//   // Uncomment the lines below, and look in the console to see if your function worked!
+//   console.log(isItLessThanTen(3));
 // console.log(isItLessThanTen(20));
 //////////////////////////////////////////////////////////////////////////
   // Write `inHighSchool` below.
+// function inHighSchool (age) {
+//   if (age <= 13) {
+//     return "Nope";
+//   } else if (age > 13) {
+//     return "High Schooler";
+//   }
+// }
 
 
 
 
-
-  // Uncomment the lines below, and look in the console to see if your function worked!
+  // // Uncomment the lines below, and look in the console to see if your function worked!
   // console.log(inHighSchool(17));
   // console.log(inHighSchool(10));
 //////////////////////////////////////////////////////////////////////////
   // Write `areTheySpeeding` below.
+// function areTheySpeeding (speed) {
+//   if (speed < 75) {
+//     return "Thanks for driving safely!";
+//   } else if (speed > 75)
+//     return "SLOW DOWN!";
+// }
 
 
 
-
-  // Uncomment the line below, and look in the console to see if your function worked!
+  // // Uncomment the line below, and look in the console to see if your function worked!
   // console.log(areTheySpeeding(60));
   // console.log(areTheySpeeding(90));
 //////////////////////////////////////////////////////////////////////////
   // Write `sum` below.
-
+// function sum (num1, num2) {
+//   return num1 + num2;
+// }
 
 
 
@@ -67,11 +95,13 @@ document.addEventListener('DOMContentLoaded', function() {
   // console.log(sum(3, 4));
 //////////////////////////////////////////////////////////////////////////
   // Write `product` below.
+// function product (num1,num2) {
+//   return num1 * num2;
+// }
 
 
 
-
-  // Uncomment the lines below, and look in the console to see if your function worked!
+  // // Uncomment the lines below, and look in the console to see if your function worked!
   // console.log(product(8, 10));
 //////////////////////////////////////////////////////////////////////////
 })
